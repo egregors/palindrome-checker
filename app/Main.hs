48fd296fec45cda6aa198513aba4e517d9b4707b
egregors <root@egregors.com>
2020-01-20 19:07:34 +1000

    palindrome checker

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import           Lib
+import           Data.Text                     as T
+import           Data.Text.IO                  as TIO
 
 main :: IO ()
-main = someFunc
+main = do
+  TIO.putStrLn "Введите слово, чтобы узнать, является ли оно палиндромом."
+
+  text <- TIO.getLine
+  let response = if isPalindrome text
+        then "Да, это палиндром"
+        else "Нет, это не палиндром."
+
+  TIO.putStrLn response
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+  ( isPalindrome
+  )
+where
+
+import qualified Data.Text                     as T
+import           Data.Char                      ( toLower
+                                                , isSpace
+                                                , isPunctuation
+                                                )
+
+stripWhiteSpace :: T.Text -> T.Text
+stripWhiteSpace text = T.filter (not . isSpace) text
+
+stripPunctuation :: T.Text -> T.Text
+stripPunctuation text = T.filter (not . isPunctuation) text
+
+preprocess :: T.Text -> T.Text
+preprocess = stripWhiteSpace . stripPunctuation . T.toLower
+
+isPalindrome :: T.Text -> Bool
+isPalindrome text = cleanText == T.reverse cleanText
+  where cleanText = preprocess text
